600192f494e1f2309f5e08be3a7cec76e2edaca5
James Barnden <mail@jamqes.com>
2018-12-01 15:55:00 +0000

    Updated docs

--- FileSync.py
+++ FileSync.py
@@ -2,15 +2,15 @@
 Basic File Sync v0.1.0
 
 Description:
-Cross-platform File Sync program that copies source file to the desination file when changes are detected.
+Cross-platform File Sync program that copies source file (from file) to the destination file (to file) when changes are detected.
 This is a hugely basic, quickly written version that needs some serious improving.  It's currently limited to working
 only with single files.
 
 In future I hope to support:
- - Multiple to & from files 
- - menu for saving and loading conifiguration and an ppliacation
- - maintained, user accessible log.
- - Support for directories
+- Multiple to & from files 
+- menu for saving and loading configuration and an application
+- maintained, user accessible log.
+- Support for directories
 
 Author:
 James Barnden
---
+++ README.md
@@ -0,0 +1,16 @@
+# Basic File Sync
+### v0.1.0
+
+Author: James Barnden
+License: MIT
+
+## Description
+Cross-platform File Sync program that copies source file (from file) to the destination file (to file) when changes are detected.
+This is a hugely basic, quickly written version that needs some serious improving.  It's currently limited to working
+only with single files.
+
+In future I hope to support:
+- Multiple to & from files 
+- menu for saving and loading configuration and an application
+- maintained, user accessible log.
+- Support for directories
